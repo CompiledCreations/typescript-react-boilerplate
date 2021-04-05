7e34972a0b479c742dc17182d6174e3b3d14c6a8
Matt Styles <matt.styles@perkinelmer.com>
2021-04-05 19:27:02 +0100

    Update storybooks, use webpack5 core, improve css modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,12 @@
 module.exports = {
+  core: {
+    builder: "webpack5"
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(sa|sc|c)ss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      exclude: /\.module\.(sa|sc|c)ss$/
-    });
-
-    config.module.rules.push({
-      test: /\.(sa|sc|c)ss$/,
-      use: ["style-loader", { loader: "css-loader", options: { modules: true } }, "sass-loader"],
-      include: /\.module\.(sa|sc|c)ss$/
-    });
-
-    // Return the altered config
-    return config;
-  }
+  addons: [
+    "@storybook/preset-scss",
+    "storybook-css-modules-preset",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
 };
